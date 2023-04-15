8f3e7798f5774b13d04dabc67299559b3c568ad9
Yevhen Abramov <abramovyevhen@MacBook-Pro-Yevhen.local>
2023-04-15 07:25:48 +0300

    fixed styles

--- docs/js/script.js
+++ docs/js/script.js
@@ -51,7 +51,7 @@ const cardTemplate = data => (`
             </p>
             <span class="show-more"></span>
         </div>
-        <div class="card-footer">
+        <div class="card-footer d-flex">
             <a href="#" class="btn btn-primary">Save to collection</a>
             <a href="#" class="btn btn-secondary">Share</a>
         </div>
@@ -107,16 +107,21 @@ const themeBtn = document.getElementById('themeChangeBtn');
 
 const toggleTheme = () => {
     const currentTheme = themeLink.dataset.current
-
-    if( currentTheme === 'light' ) {
-        themeLink.setAttribute('href', 'css/theme_dark.css');
-        themeLink.dataset.current = 'dark';
-        document.body.className = 'dark';
-    } else {
-        themeLink.setAttribute('href', 'css/theme_light.css');
-        themeLink.dataset.current = 'light';
-        document.body.className = 'light';
-    }
+    const newTheme = currentTheme === 'light' ? 'dark' : 'light';
+    themeLink.setAttribute('href', `css/theme_${newTheme}.css`);
+    themeLink.dataset.current = newTheme;
+    document.body.className = newTheme;
 };
 
 themeBtn.addEventListener('click', toggleTheme);
+
+if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
+    themeLink.setAttribute('href', `css/theme_dark.css`);
+    themeLink.dataset.current = 'dark';
+    document.body.className = 'dark';
+}
+window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', event => {
+    themeLink.setAttribute('href', `css/theme_${event.matches ? "dark" : "light"}.css`);
+    themeLink.dataset.current = event.matches ? "dark" : "light";
+    document.body.className = event.matches ? "dark" : "light";
+});
